85579177137639e20a73a81793d68578155b4e1a
Milkenm <miguel.campos.johnny@gmail.com>
2020-07-07 21:27:04 +0100

    Remove useless code

--- ProjetoFinal Console/main.cpp
+++ ProjetoFinal Console/main.cpp
@@ -9,65 +9,7 @@
 #include "funcoes.h"
 #include "livros.h"
 
-// Guarda cenas no ficheiro
-void GuardarNoFicheiro()
-{
-   char nome[50];
-   int numCon, i, num;
-
-   printf("Número de concelhos: ");
-   scanf("%d", &num);
-
-   FILE *file = (fopen("dados\\test.txt", "w"));
-   if(file == NULL)
-   {
-       printf("Error! File not found.");
-       getch();
-   }
-
-   for(i = 0; i < num; ++i)
-   {
-      printf("Concelho %d\nIntroduza o nome: ", i+1);
-      scanf("%s", nome);
-
-      printf("Introduza o numero do concelho: ");
-      scanf("%d", &numCon);
-
-      fprintf(file,"\nNome: %s \nNumeroConcelho: %d \n", nome, numCon);
-   }
-
-   fclose(file);
-}
-
-// Abre e lê as cenas do ficheiro
-void LerFicheiro()
-{
-	int c;
-	FILE *file = fopen("dados\\test.txt", "r");
-
-	if (file)
-    {
-        while ((c = getc(file)) != EOF)
-
-        putchar(c);
-    	fclose(file);
-	}
-}
-
-void Testing()
-{
-    Livro *l = WizardCriarLivro();
-
-	GuardarNoFicheiro();
-	LerFicheiro();
-}
-
 int main()
 {
-    // Imprimir chars portuguêses como 'ç', 'á', ...
-    setlocale(LC_ALL, "Portuguese");
-
-    Testing();
-
 	return 0;
 }
